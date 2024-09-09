a1be8de5efab4e6f97e9d92b941bd21e47fd95f9
Ravi <ravisankar@campx.in>
2024-09-09 05:46:39 +0530

    data base connected

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-console.log("this is test")
+import { config } from 'dotenv';
+config({
+    path:'./.env'
+})
+
+console.log(process.env.PORT)
+
+import { app } from './app.js';
+import connectDB from './db/index.js';
+
+
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) =>  {
+    console.log("MongoDB connection failed !!!", err);
+})
+
+
+
+
+
+
+
+//another way to connect the database
+// ;( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log("Error:", error);
+//             throw error
+//         })
+      
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
+
+//     } catch ( error ) {
+//         console.error("ERROR: ", error)
+//         throw err
+//     }
+// }) ()
